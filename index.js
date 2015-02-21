9a63a3a00f3ef6ee8141d18d01ee796a3b007b6d
Bifuer <bifuer@odeux.com>
2015-02-21 11:47:26 +0100

    fixes from test

--- index.js
+++ index.js
@@ -5,89 +5,76 @@ module.exports = ObjectArray;
 function ObjectArray(){
   var args = Array.prototype.slice.call(arguments);
   this.splice.apply(this,[0,0].concat(args));
+  this.pid = 'id';
 }
-ObjectArray.prototype = [];
+  ObjectArray.prototype = [];
+  ObjectArray.prototype.constructor = Array;
 
-ObjectArray.prototype.constructor = ObjectArray;
-
-ObjectArray.prototype.insert = function(element,index,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  if(Math.floor(index) === parseFloat(index,10) && index>=0 && index<=this.length){
-    this.splice(index,0,element);
-  } else {
-    this.push(element);
-  }
-  return this;
-};
-ObjectArray.prototype.insertAfter = function(element,index,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  index++;
-  if(Math.floor(index) === parseFloat(index,10) && index>=0 && index<=this.length){
-    this.splice(index,0,element);
-  } else {
-    this.push(element);
-  }
-  return this;
-};
-ObjectArray.prototype.get = function(index,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  if(Math.floor(index) === parseFloat(index,10) && index>=0 && index<=this.length){
-    return this[index];
-  }
-};
-ObjectArray.prototype.search = function(index,pid){
-  pid = pid || 'id';
-  for(var i=0,l=this.length;i<l;i++){
-    if(this[i][pid] === index){
-      return i;
+  ObjectArray.prototype.insert = function(element,index,pid,after){
+    index = typeof index === 'string' ? this.search(index,pid) : index === -1 ? this.length : index;
+    index += after ? 1 : 0;
+    if(Math.floor(index) !== parseFloat(index,10) || index<0 || index>this.length){
+      this.push(element);
+    } else {
+      this.splice(index,0,element);
     }
-  }
-};
-ObjectArray.prototype.move = function(index,indexTo,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  if(!(Math.floor(index) === parseFloat(index,10) && index>=0 && index<this.length)){
-    return;
-  }
-  var copy = this[index];
-  if(typeof indexTo === 'string'){indexTo = this.search(indexTo,pid);}
-  if(index == indexTo || index==indexTo-1){
-    return;
-  }
-  if(Math.floor(indexTo) === parseFloat(indexTo,10) && indexTo>=0 && indexTo<=this.length){
+    return this;
+  };
+
+  ObjectArray.prototype.insertAfter = function(element,index,pid){
+    return this.insert(element,index,pid,true);
+  };
+
+  ObjectArray.prototype.add = ObjectArray.prototype.insert;
+
+  ObjectArray.prototype.addAfter = ObjectArray.prototype.insertAfter;
+
+  ObjectArray.prototype.get = function(index,pid){
+    index = typeof index === 'string' ? this.search(index,pid) : index === -1 ? this.length : index;
+    if(Math.floor(index) !== parseFloat(index,10) || index<0 || index>this.length){
+      return;
+    } else {
+      return this[index];
+    }
+  };
+
+  ObjectArray.prototype.search = function(index,pid){
+    pid = pid || this.pid || 'id';
+    for(var i=0,l=this.length;i<l;i++){
+      if(this[i][pid] === index){
+        return i;
+      }
+    }
+  };
+
+  ObjectArray.prototype.move = function(index,indexTo,pid,after){
+    index = typeof index === 'string' ? this.search(index,pid) : index === -1 ? this.length : index;
+    if(Math.floor(index) !== parseFloat(index,10) || index<0 || index>this.length){
+      return false;
+    }
+    indexTo = typeof indexTo === 'string' ? indexTo.search(indexTo,pid) : indexTo === -1 ? this.length : indexTo;
+    if(Math.floor(indexTo) !== parseFloat(indexTo,10) || indexTo<0 || indexTo>this.length){
+      return false;
+    }
+    indexTo += after ? 1 : 0;
+    if(index == indexTo || index==indexTo-1){
+      return true;
+    }
+    var copy = this[index];
     this.splice(indexTo, 0, copy);
     this.splice(index > indexTo ? index+1 : index, 1);
-  } else {
-    this.push(copy);
-    this.splice(index, 1);
-  }
-  return true;
-};
-ObjectArray.prototype.moveAfter = function(index,indexTo,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  if(!(Math.floor(index) === parseFloat(index,10) && index>=0 && index<this.length)){
-    return;
-  }
-  var copy = this[index];
-  if(typeof indexTo === 'string'){indexTo = this.search(indexTo,pid);}
-  if(index == indexTo || index==indexTo+1){
-    return;
-  }
-  if(Math.floor(indexTo) === parseFloat(indexTo,10) && indexTo>=0 && indexTo<=this.length){
-    indexTo++;
-    this.splice(indexTo, 0, copy);
-    this.splice(index > indexTo ? index+1 : index, 1);
-  } else {
-    this.push(copy);
-    this.splice(index, 1);
-  }
-  return true;
-};
-ObjectArray.prototype.remove = function(index,pid){
-  if(typeof index === 'string'){index = this.search(index,pid);}
-  if(Math.floor(index) === parseFloat(index,10) && index>=0 && index<=this.length){
-    this.splice(index, 1);
     return true;
-  } else {
-    return undefined;
-  }
-};
+  };
+
+  ObjectArray.prototype.moveAfter = function(index,indexTo,pid){
+    this.move(index,indexTo,pid,true);
+  };
+
+  ObjectArray.prototype.remove = function(index,pid){
+    index = typeof index === 'string' ? this.search(index,pid) : index === -1 ? this.length : index;
+    if(Math.floor(index) !== parseFloat(index,10) || index<0 || index>this.length){
+      return false;
+    } else {
+      this.splice(index, 1);
+    }
+  };
